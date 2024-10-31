a99a6d8055ccf99eb9bb9859f598cdb52bc0fa7b
Bob K. <rwkramer@ysu.edu>
2024-10-31 13:41:31 -0400

    updated tic-tac-toe project

--- Projects/3 - Tic-Tac-Toe/project3.cc
+++ Projects/3 - Tic-Tac-Toe/project3.cc
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <random>
 
 using namespace std;
 
 const char
   LETTERS[3] = {'O',' ','X'};
+const int
+  lines[8][3] = { {0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6} };
 
 int
-  board[9];
+  board[9]={-1,0,1,1,1,-1,-1,0,0};
+ 
+//=============================================================================
+// int score(int cell1,int cell2,int cell3)
+//   Return sum of cells' contents
+//
+// Parameters
+//   cell1,cell2,cell3 - cells (0-8) to sum
+//
+// Returns
+//   board[cell1] + board[cell2] + board[cell3]
+//
+
+int score(int cell1,int cell2,int cell3) {
+
+  return board[cell1] + board[cell2] + board[cell3];
+}
  
 //=============================================================================
 // void displayBoard()
@@ -94,6 +113,14 @@ void displayBoard() {
 
 bool playerWins(int playerLetter) {
 
+  // check every line
+  for (auto line : lines) {
+  
+    // check score for that line
+    if (score(line[0],line[1],line[2]) * playerLetter == 3)
+      return true;
+  }
+
   return false; // placeholder
 }
   
@@ -111,6 +138,18 @@ bool playerWins(int playerLetter) {
 //
 
 bool canWin(int playerLetter,int &winningSquare) {
+
+  // check every line
+  for (auto line : lines) {
+  
+    // check score for that line
+    if (score(line[0],line[1],line[2]) * playerLetter == 2) {
+      if (board[line[0]] == 0)
+        winningSquare = line[0];
+      // repeat for line[1] and line[2]
+      return true;
+    }
+  }
 
   return false; // placeholder to make compiler less unhappy
 }
@@ -181,10 +220,19 @@ int main() {
   int
     humanPlayer,
     computerPlayer;
+  random_device
+    rd;
+  mt19937
+    mt(rd());
+  uniform_int_distribution<>
+    dis(0,1);
     
   displayBoard();
   
   // randomly choose X player
+  humanPlayer = 1 - 2 * dis(mt);
+  computerPlayer = -humanPlayer;
+  isHumanTurn = (humanPlayer == 1);
   
   // loop at most 9 times... for loop here
   
@@ -200,6 +248,8 @@ int main() {
       isHumanTurn = !isHumanTurn;
       
   // end loop
+  
+  // if we get here, it's a draw
 
   // all done
   return 0;
